f5f9844e2fd05832a538aeafb7724d23b6fb7676
Yasha Bubnov <ybubnov@lyft.com>
2020-06-13 18:11:27 +0300

    Parse options from environment variable.

--- lib/hectocorn.rb
+++ lib/hectocorn.rb
@@ -1,5 +1,17 @@
+require 'json'
+
+
 module Hectocorn
   require 'hectocorn/action'
+
+  HECTODNS_ENVIRON_VAR= "hectodns.options"
+
+  @@opts = nil
+
+  def self.options
+    @@opts = @@opts || self.parse_options
+    return @@opts
+  end
 
   def self.infinite_loop(&block)
     action = Hectocorn::Action.new
@@ -9,5 +21,12 @@ module Hectocorn
     while true
       action.run
     end
+  end
+
+private
+
+  def self.parse_options
+    opts = ENV[HECTODNS_ENVIRON_VAR]
+    return opts ? JSON.parse(opts) : {}
   end
 end
